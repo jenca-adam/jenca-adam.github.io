dc0348781af7df7ef1075e63605d8e61e5a6e135
Adam Jenča <jenca.adam@gmail.com>
2021-04-28 20:10:52 +0200

    removed js directory

--- js/hannhoverjs/hannhover.js
+++
@@ -1,5 +0,0 @@
-$('hannhover-result').hide();
-$('.hannhover-container .hannhover-link').mouseenter(function (e){$('.hannhover-result').load($(e.target).attr('href')+' body .hannhover-content')})
-$('.hannhover-container .hannhover-link').mouseleave((function (e){$('.hanhover-result').hide(500)}))
-
-		
