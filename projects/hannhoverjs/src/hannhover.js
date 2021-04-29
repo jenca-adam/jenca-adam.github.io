37aac1d5ae5697954078af6bb6f203d0d7e6b1d8
Adam Jenča <jenca.adam@gmail.com>
2021-04-29 19:27:35 +0200

    ?

--- projects/hannhoverjs/src/hannhover.js
+++ projects/hannhoverjs/src/hannhover.js
@@ -1,5 +1,5 @@
 $('#hannhover-result').hide();
-$('#hannhover-container .hannhover-link').mouseenter(function (e){$('#hannhover-result').load($(e.target).attr('hh-location'));$('#hannhover-result').show(500)});
-$('#hannhover-container .hannhover-link').mouseleave((function (e){$('#hanhover-result').hide(500)}));
+$('#hannhover-container .hannhover-link').mouseenter(function (e){$('#hannhover-result').load($(e.target).attr('hh-location'));$('#hannhover-result').show()});
+$('#hannhover-container .hannhover-link').mouseleave((function (e){console.log('test');$('#hanhover-result').hide()}));
 
 		
