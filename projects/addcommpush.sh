f764e52b2e328adc12b33642dbc2264f291b06e3
Adam Jenča <jenca.adam@gmail.com>
2021-04-29 19:38:49 +0200

    bb

---
+++ projects/addcommpush.sh
@@ -0,0 +1,3 @@
+git add --all
+git commit -m bb
+git push
--- projects/hannhoverjs/src/hannhover.js
+++ projects/hannhoverjs/src/hannhover.js
@@ -1,3 +1,7 @@
+function hideResult(){
+	$('#hanhover-result').delay(10).hide()
+}
+	
 $('#hannhover-result').hide();
 $('#hannhover-container .hannhover-link').mouseenter(function (e){$('#hannhover-result').delay(100).load($(e.target).attr('hh-location'));$('#hannhover-result').show()});
 $('#hannhover-container .hannhover-link').mouseleave((function (){$('#hanhover-result').hide()}));
