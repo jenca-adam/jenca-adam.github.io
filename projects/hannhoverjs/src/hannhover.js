878f1a9aa2559cee9f30f2d56868dd868047a40e
Adam Jenča <jenca.adam@gmail.com>
2021-04-29 19:36:19 +0200

    bsahbads

--- projects/hannhoverjs/src/hannhover.js
+++ projects/hannhoverjs/src/hannhover.js
@@ -1,5 +1,5 @@
 $('#hannhover-result').hide();
-$('#hannhover-container .hannhover-link').mouseenter(function (e){$('#hannhover-result').load($(e.target).attr('hh-location'));$('#hannhover-result').show()});
+$('#hannhover-container .hannhover-link').mouseenter(function (e){$('#hannhover-result').delay(100).load($(e.target).attr('hh-location'));$('#hannhover-result').show()});
 $('#hannhover-container .hannhover-link').mouseleave((function (){$('#hanhover-result').hide()}));
 
 		
